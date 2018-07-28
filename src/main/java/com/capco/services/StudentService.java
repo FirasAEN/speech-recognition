a48a2a6e82a8c4071296f2d1b2d09df7e9d06ab5
Firas AEN <noOne@nowehere.com>
2018-07-28 17:37:26 +0200

    Creates 'StudentService' service class
    Signed-off-by: Firas AEN <noOne@nowehere.com>

---
+++ src/main/java/com/capco/services/StudentService.java
@@ -0,0 +1,25 @@
+package com.capco.services;
+
+import com.capco.repositories.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Firas on 7/28/2018.
+ */
+@Service
+public class StudentService {
+    private final static Logger Log = LoggerFactory.getLogger(StudentService.class);
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
+    public void getStudents(){
+        studentRepository.getAllStudents();
+    }
+}
