cae8e082819a4e3dffc496bab076a422a8a03052
Firas AEN <noOne@nowehere.com>
2018-08-18 17:37:17 +0200

    Adds a student repository test
    Signed-off-by: Firas AEN <noOne@nowehere.com>

---
+++ src/test/java/com/capco/repositories/JdbcStudentRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.capco.repositories;
+
+import com.capco.entities.Student;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Firas on 8/5/2018.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = com.capco.config.AppConfig.class)
+public class JdbcStudentRepositoryTest {
+
+    @Autowired
+    JdbcStudentRepository repo;
+
+    @Test
+    public void getStudentWithMajor() throws Exception {
+        Student student = repo.getStudentWithMajor(1);
+        assertEquals(student.getId(), 1);
+    }
+
+}
